1c7be693da09aebd4aed110bbf771722b5910a6e
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-20 17:24:28 +0530

    solved 1st problem

---
+++ chapter1_Problem1.py
@@ -0,0 +1,38 @@
+# print twinkle twinkle poem 
+# print ("Twinkle, twinkle, little star\n How I wonder what you are\nUp above the world so high\nLike a diamond in the sky\nTwinkle, twinkle, little star\nHow I wonder what you are")
+# print("\nTwinkle, twinkle, little star\nHow I wonder what you are\n up above the world so high\nLike a diamond in the sky\nTwinkle, twinkle, little star\nHow I wonder what you are")
+
+
+# print("\n\nTwinkle, twinkle, little star\nHow I wonder what you are\nUp above the world so high\nLike a diamond in the sky\nTwinkle, twinkle, little star\nHow I wonder what you are")
+
+print(""" Song by
+CoComelon
+
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+
+Twinkle, twinkle, little star
+How I wonder what you are
+Up above the world so high
+Like a diamond in the sky
+Twinkle, twinkle, little star
+How I wonder what you are
+""")
