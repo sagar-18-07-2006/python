1e171b42e16bed70f90a78aa9f47dcefc4198be5
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-21 12:05:02 +0530

    imput

---
+++ Chapteer_2_Input.py
@@ -0,0 +1,9 @@
+a = input ("Enter Number 1 ")
+b=input("Enter number 2 ")
+c= a+b
+print (c)
+##input will be in form of string 
+d = int(input ("Enter Number 1 "))
+s=int(input("Enter number 2 "))
+q= s+d
+print (q)
