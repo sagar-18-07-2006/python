726a691567f4fea2f0789b87bc57c11ee6c50c2a
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-21 11:12:12 +0530

    operators

---
+++ Chapter_2_operators.py
@@ -0,0 +1,20 @@
+#Arthimetic Operators
+a=7
+b=1
+print(a+b)
+c=a+b
+print(c)
+a=4-2
+#ASSSSIGNED  a 4-2
+b=6
+b-=6
+#b-=6  => b=b-6
+
+print (b)
+print (a)
+#comparrision operators
+d=5>4
+print((d))# comparission operators always return boolean
+#logical Operators
+e =True or False
+print(e)
