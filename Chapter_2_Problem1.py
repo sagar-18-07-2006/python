8bac58e62a8051e53a303ae9cf6cb211f37d2575
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-05-21 12:14:54 +0530

    done for today

---
+++ Chapter_2_Problem1.py
@@ -0,0 +1,5 @@
+d = int(input ("Enter Number 1 "))
+s=int(input("Enter number 2 "))
+q= s+d
+print (q)
+#add two numbers
---
+++ Chapter_2_Problem2.py
@@ -0,0 +1,3 @@
+a=int(input ("Enter number to be divided "))
+b=int(input ("Enter number  from to be divided "))
+print (a%b)
