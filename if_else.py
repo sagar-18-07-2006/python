5bd0f33ffc7ad18eaeac965cffe42a23bdab3929
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-02 07:39:40 +0530

    do

--- if_else.py
+++ if_else.py
@@ -1,6 +1,11 @@
 age =int(input("enter age "))
 if age>18 :
-    print("you can drive")
+  
+    if age>=65:
+         print("you can not drive")
+    else :
+          print("you can drive")
+        
 else:
     print("you can not drive")
         
