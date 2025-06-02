76cd5fbe5bbe9e5f2889add730fe408e2ef02068
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-02 07:37:19 +0530

    do

---
+++ if_else.py
@@ -0,0 +1,6 @@
+age =int(input("enter age "))
+if age>18 :
+    print("you can drive")
+else:
+    print("you can not drive")
+        
---
+++ special_operator.py
@@ -0,0 +1,7 @@
+name="sagar"
+print(("r" in name))
+a=55
+b=55
+print(a is b)
+c=25
+print(a is c)
