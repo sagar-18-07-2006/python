2bd85a3d00d48f0ca5ed3ea0df7d43567415a37b
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-02 07:53:21 +0530

    do

---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+num =(int(input("enter a bumber : ")))
+if num>0:
+    print('number is positive')
+    
+elif num==0:
+    print("it is zero") 
+else: 
+    print("it is not negitive")   
---
+++ maximum_in_list.py
@@ -0,0 +1,7 @@
+marks=[10,2,0,30,40,50,60,70,80,90,100]
+
+highest=marks[0]
+for i in marks:
+    if i>highest:
+        highest =i
+print("highest marks is",highest)
