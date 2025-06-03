71d5edb020963905c6aa0c6c1ad51f4c88d75e6d
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-03 23:19:26 +0530

    do

---
+++ print_vowels_in_a_string.py
@@ -0,0 +1,6 @@
+text=input("enter text")
+
+for i in text:
+    if (i=="a")or (i=="e")or (i=="i")or (i=="o")or(i=="u") :
+        print(i)
+        
---
+++ reverse_of_a_string_pallidrome.py
@@ -0,0 +1,5 @@
+text=input("enter a text")
+if(text== text[-1:0:-1]):
+    print("it is a pallidrome" )
+else:
+    print("it is not a pallidrome" )
