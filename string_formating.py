36aa9711f3659399262d2f85e607a33d04b2758a
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-03 22:13:55 +0530

    do

---
+++ string_formating.py
@@ -0,0 +1,6 @@
+name=input("enter name ")
+age =input("enter age ")
+print("my name is  ",name, " and age is " ,age)
+#string formatting
+print("Hey my name is {} and my age is {}".format(name,age) )
+print(f"Hey my name is {name} and my age is {age}" )
