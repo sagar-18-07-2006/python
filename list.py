e3eff823465caf1d1e23709163bf7ff65266f13b
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-04 17:46:29 +0530

    do

---
+++ list.py
@@ -0,0 +1,11 @@
+l=[46,989684,66,6,66,42,644]
+m=list()
+print(type(m))
+
+print(type(l))
+for i in m:
+    print(i)
+    print ("hi")
+for i in l:
+    print(i)
+    
--- string_formating.py
+++ string_formating.py
@@ -3,4 +3,3 @@ age =input("enter age ")
 print("my name is  ",name, " and age is " ,age)
 #string formatting
 print("Hey my name is {} and my age is {}".format(name,age) )
-print(f"Hey my name is {name} and my age is {age}" )
