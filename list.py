8e31a97c99fd3c3377a92974b0fabe7602eae7c8
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-04 18:30:09 +0530

    do

--- list.py
+++ list.py
@@ -1,41 +1,63 @@
-l=[46,989684,66,6,66,42,644]
-m=list()
-print(type(m))
+# l=[46,989684,66,6,66,42,644]
+# m=list()
+# print(type(m))
 
-print(type(l))
-# iterabklke
-for i in m:
-    print(i)
-    print ("hi")
-for i in l:
-    print(i)
-#acccsesing
-l1=[1,2,3,4,5,6,7,8,9,10]
+# print(type(l))
+# # iterabklke
+# for i in m:
+#     print(i)
+#     print ("hi")
+# for i in l:
+#     print(i)
+# #acccsesing
+# l1=[1,2,3,4,5,6,7,8,9,10]
 
-print(l1[0])
-print(l1[-1])
-print(l1[-(len(l1))])
+# print(l1[0])
+# print(l1[-1])
+# print(l1[-(len(l1))])
 
-#mutubaility
-print(id(l1))
-l1[0]=22
-print(l1[0])
-print(id(l1))
+# #mutubaility
+# print(id(l1))
+# l1[0]=22
+# print(l1[0])
+# print(id(l1))
 
-#list slicing
-print(len(l1))
-for i in l1[0:3]:
-    print (i)
-# inbuilt methods
+# #list slicing
+# print(len(l1))
+# for i in l1[0:3]:
+#     print (i)
+# # inbuilt methods
+# budget =[100,200,150,100]  #million dollar
+# print(budget.count(100))
+# print(budget.index(100))
+# name1=["sagar","hardik","yashpalsinngh","darshita "]
+# for i in  name1:
+#     print(i)
+# print()
+# heart=name1.pop()
+# for i in  name1:
+#     print(i)
+# print()
+# print(heart)
 budget =[100,200,150,100]  #million dollar
-print(budget.count(100))
-print(budget.index(100))
-name=["sagar","hardik","yashpalsinngh","darshita "]
-for i in  name:
+budget.sort()
+for i in budget:
     print(i)
-print()
-heart=name.pop()
-for i in  name:
+name=["dgb656,",244,True,False,174786,"ss"]
+# # name.sort()
+# # for i in name:
+# #     print(i)  
+# #it eill give typo error
+name = ["dgb656,", 244, True, False, 174786, "ss"]
+
+strings = sorted([x for x in name if isinstance(x, str)])
+numbers = sorted([x for x in name if isinstance(x, (int, float, bool))])
+
+# Combine or print separately
+for i in strings + numbers:
     print(i)
-print()
-print(heart)
+    1
+s=["sagar","radhe"]
+s.insert(1,"ram")
+for i in s:
+    print(i)
