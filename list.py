ac9ae1936892ffedd79d1c81242dedf620c35765
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-04 18:06:50 +0530

    do

--- list.py
+++ list.py
@@ -1,11 +1,32 @@
-l=[46,989684,66,6,66,42,644]
-m=list()
-print(type(m))
+# l=[46,989684,66,6,66,42,644]
+# m=list()
+# print(type(m))
 
-print(type(l))
-for i in m:
-    print(i)
-    print ("hi")
-for i in l:
-    print(i)
-    
+# print(type(l))
+# # iterabklke
+# for i in m:
+#     print(i)
+#     print ("hi")
+# for i in l:
+#     print(i)
+# #acccsesing
+# l1=[1,2,3,4,5,6,7,8,9,10]
+
+# print(l1[0])
+# print(l1[-1])
+# print(l1[-(len(l1))])
+
+# #mutubaility
+# print(id(l1))
+# l1[0]=22
+# print(l1[0])
+# print(id(l1))
+
+# #list slicing
+# print(len(l1))
+# for i in l1[0:3]:
+#     print (i)
+# inbuilt methods
+budget =[100,200,150,100]  #million dollar
+print(budget.count(100))
+print(budget.index(100))
