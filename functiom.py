9babfb1fcd581e3712ae681c705b891fa04495e4
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-06 22:09:42 +0530

    edit

---
+++ functiom.py
@@ -0,0 +1,3 @@
+def add_three(num1, num2, num3):
+   sum_three = num1 + num2 + num3
+   return sum_three
--- string_formating.py
+++ string_formating.py
@@ -4,3 +4,4 @@ print("my name is  ",name, " and age is " ,age)
 #string formatting
 print("Hey my name is {} and my age is {}".format(name,age) )
 print("Hey my name is {} and my age is {}".format(name,age) )
+print(f"Hey my name is {name} and my age is {age}" )
