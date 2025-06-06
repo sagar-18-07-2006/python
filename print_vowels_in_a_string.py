3bfd1cc5c707da4502292aa40b4d0bf43dd30c68
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-06 21:17:36 +0530

    edit

--- print_vowels_in_a_string.py
+++ print_vowels_in_a_string.py
@@ -9,3 +9,8 @@ for i in text:
     if (i=="a")or (i=="e")or (i=="i")or (i=="o")or(i=="u") :
         print(i)
         
+
+for i in text:
+    if (i=="a")or (i=="e")or (i=="i")or (i=="o")or(i=="u") :
+        print(i)
+        
