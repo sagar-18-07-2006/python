1fd5b0b569005155930c9966c64cd547448580df
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-06 21:17:55 +0530

    edit

--- star_square.py
+++ star_square.py
@@ -1,12 +1,12 @@
-# n=int(input("ener side length"))
-# i =0
-# while(i<n):
-    # j=0
-#     while(j<n):
-#         print("*",end=' ')
-#         j+=1
-#     print(" ")
-#     i+=1
+n=int(input("ener side length"))
+i =0
+while(i<n):
+    j=0
+    while(j<n):
+        print("*",end=' ')
+        j+=1
+    print(" ")
+    i+=1
 
 n = int(input("Enter side length: "))
 i = 0
