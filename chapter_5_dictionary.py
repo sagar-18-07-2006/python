c11ee3836d4647f4ed603e68962b4802660221bd
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-10 12:46:58 +0530

    learning dictionary

---
+++ chapter_5_dictionary.py
@@ -0,0 +1,9 @@
+marks ={
+    "harry" : 100,
+    "sagar": 100,
+    "darshita":100,
+    "hardik" :100,
+    "kartavya" : 14}
+print(marks,type(marks))
+# print(marks[0])
+print(marks["sagar"])
---
+++ chapter_5_dicttionary_methods.py
@@ -0,0 +1,10 @@
+marks ={
+    "harry" : 100,
+    "sagar": 100,
+    "darshita":100,
+    "hardik" :100,
+    "kartavya" : 14}
+print(marks.items())
+
+print(marks.keys())
+print(marks.values())
