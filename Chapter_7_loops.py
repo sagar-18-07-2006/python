f6ec71009841fa839a932e872c0743a08f1782da
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-17 20:16:45 +0530

    problem 1 donr

--- Chapter_7_loops.py
+++ Chapter_7_loops.py
@@ -1,8 +1,12 @@
+# for i in range(9):
+#     print(i)
 # for i in range(0,100):
 #     print(i+1)
-i=0
-while i <7:
-    print(i+1)
-    i=i+1
+# i=0
+# while i <7:
+#     print(i+1)
+#     i=i+1
 
-    
+l=["sagar",141,11.2]
+for i in l:
+    print(i)
