d935c51b78d170e5ca8675bc08499df513fe0f69
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-18 09:26:59 +0530

    did starr pattern

---
+++ Chapter_7_problem5.py
@@ -0,0 +1,11 @@
+n=int(input("Enter siae of triangle: "))
+for i in range(n):
+    j=n-i
+    while j>0:
+        print(end=" ")
+        j-=1
+        
+
+    for i in range (i+1):
+        print("*",end=" ")
+    print()
--- chapter_7_problem4.py
+++ chapter_7_problem4.py
@@ -1,6 +1,7 @@
 n =int(input("Enter a number: "))
 if (n>2):
-     for i in (2,n):
+     for i in range(2,n):
          if (n%i == 0):
+           print(i)
            print("number is not a prime ")
            break
