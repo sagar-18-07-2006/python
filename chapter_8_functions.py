64b1829295710338aa75300cba8fd16fed3097cb
Sagar Maheshwari <Sagarmaheshwari650@gmail.com>
2025-06-19 21:49:01 +0530

    Done

---
+++ chapter_8_functions.py
@@ -0,0 +1,22 @@
+# a=12
+# b=45
+# c=56
+# avg=(a+b+c)/3
+# print(avg)
+# a=3
+# b=1
+# c=2
+# avg=(a+b+c)/3
+# print(avg)
+def avg():
+    a=int(input("Enter a number: "))
+    b=int(input("Enter a number: "))
+    c=int(input("Enter a number: "))
+    avg = (a+b+c)/3
+    print(avg)
+avg()
+avg()
+avg()
+avg()# this is function call
+avg()
+avg()
