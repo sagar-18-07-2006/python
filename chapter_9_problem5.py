ffac38058ee38afdee5160f9fe672f9b5330006c
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-06-30 22:03:49 +0530

    did sensoring words from list of bad words "

--- chapter_9_problem5.py
+++ chapter_9_problem5.py
@@ -2,7 +2,7 @@ words = ["donkey","pagal","bawle"]
 with open ("donkey.txt" ,"r") as f:
     content =f.read()
 for i in words:
-    content=content.replace(i,"####")
+    content=content.replace(i,"*"*len(i))
 print(content)
 with open ("donkey.txt" ,"w") as f:
     f.write(content)
