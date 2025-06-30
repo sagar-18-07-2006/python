88738e475efee16c1bff649e90e244aa43399e50
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-06-30 22:02:14 +0530

    sensoring best words from list of bad words

--- chapter_9_problem3.py
+++ chapter_9_problem3.py
@@ -1,6 +1,6 @@
 def multiplytable(n):
    
-    with open(f"maths_table_{n}.txt","a") as f:
+    with open(f"tabels/maths_table_{n}.txt","a") as f:
         f.write((f"Table of {n} ==>"))
         for i in range (1,11):
             f.write(f"\n{n} X {i} ={n*i}\n")
---
+++ chapter_9_problem5.py
@@ -0,0 +1,8 @@
+words = ["donkey","pagal","bawle"]
+with open ("donkey.txt" ,"r") as f:
+    content =f.read()
+for i in words:
+    content=content.replace(i,"####")
+print(content)
+with open ("donkey.txt" ,"w") as f:
+    f.write(content)
