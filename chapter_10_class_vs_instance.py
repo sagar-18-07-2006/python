e26277405c587563d182555f514abce1c2427db2
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-02 22:33:09 +0530

    Did theinstance vs classs

---
+++ chapter_10_class_vs_instance.py
@@ -0,0 +1,8 @@
+# class is bluepriint (placeholder) 
+class Employee:
+    
+    language ="py" #this is class attribute
+    salary =120000#this is class attribute
+sagar =Employee()# This is an instance attribute
+sagar.language ="CPP"
+print(sagar.language,sagar.salary)
