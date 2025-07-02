a955c15a130ddaea154fe33a7b7902f79b39028f
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-02 22:29:48 +0530

    doing oops in python

---
+++ chapter_10_OOPs.py
@@ -0,0 +1,10 @@
+# class is bluepriint (placeholder) 
+class Employee:
+    
+    language ="py" #this is class attribute
+    salary =120000#this is class attribute
+sagar =Employee()# This is an instance attribute
+print(sagar.language,sagar.salary)
+rohan =Employee()
+print(rohan.salary,rohan.language)
+# herename is object attribute  and salary and language are class attribute as they directlt belong to clash
