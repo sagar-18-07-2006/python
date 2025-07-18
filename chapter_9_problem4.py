49c8645013e64cca7e9ad3a31a893f0e7995b20c
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-07-18 20:25:55 +0530

    did some mischief

--- chapter_9_problem4.py
+++ chapter_9_problem4.py
@@ -1,7 +1,8 @@
 word = "donkey"
 with open ("donkey.txt" ,"r") as f:
     content =f.read()
-contentNew=content.replace("donkey","####")
+contentNew=content.replace("Nandini","donkey")
 print(contentNew)
 with open ("donkey.txt" ,"w") as f:
     f.write(contentNew)
+print("Nandini is Donkey ")
