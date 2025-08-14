6f16bc85d5024c098bd074c3d200f249e9898abf
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-08-14 09:54:25 +0530

    Did these in course by gyaneshwar sir

---
+++ gyaneshwar sir/evenOddNumber.py
@@ -0,0 +1,7 @@
+n =int(input("Enter the Number to check: "))
+if n%2==0:
+    print(f"Number {n} is even")
+else:
+    print(f"Number {n} is Odd")
+
+    
---
+++ gyaneshwar sir/typeofTriangle.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the First Side of triangle: "))
+b=int(input("Enter the Second Side of triangle: "))
+c=int(input("Enter the Third Side of triangle: "))
+if(a==b):
+    if(a==c):
+        print("Triangle is Equilateral")
+    else:
+        print("Triangle is Isoceles")
+    
