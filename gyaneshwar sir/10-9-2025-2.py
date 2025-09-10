b04573b676dc51cfe34a17be33e060cbcb67c19c
Sagar  Maheshwari <sagarmaheshwari650@gmail.com>
2025-09-10 23:49:29 +0530

    did

--- gyaneshwar sir/10-9-2025-2.py
+++ gyaneshwar sir/10-9-2025-2.py
@@ -1,9 +1,16 @@
-class vehicle:
-    def __init__(self,milege,max_speed):
-        self.max_speed=max_speed
-        self.milege=milege
-    def display(self):
-        print(f"{self.milege} is the milege and have max speed of {self.max_speed}")
-car=vehicle(16,160)
-car.display()
-        
+# # class vehicle:
+# #     def __init__(self,milege,max_speed):
+# #         self.max_speed=max_speed
+# #         self.milege=milege
+# #     def display(self):
+# #         print(f"{self.milege} is the milege and have max speed of {self.max_speed}")
+# # car=vehicle(16,160)
+# # car.display()
+# l1={1,2,3,8,9,10,4,5,6,7}
+# l2={2,1,3,8,9,10,4,5,6,7}
+# print(l1== l2)
+z1 = 3 + 4j
+z2 = complex(5, -2)   # another way
+
+print(type(z1))       # (3+4j)
+print(z2)       # (5-2j)
